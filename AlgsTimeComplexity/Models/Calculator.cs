1ccadd4de4694bb64dd0c9aa2a752f71f855e307
Dying <dyingvoid@gmail.com>
2023-09-24 11:37:57 +0500

    improved structure

---
+++ AlgsTimeComplexity/Models/Calculator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace AlgsTimeComplexity.Models;
+
+public class Calculator
+{
+    public void Calculate(int size, MethodInfo methodInfo, ObservableCollection<double> plot)
+    {
+        if (plot.Count != 0)
+            plot.Clear();
+
+        var parameters = GenerateParameters(methodInfo, size);
+
+        for (var i = 0; i < size; i++)
+        {
+            var closureTemp = i;
+
+            Task.Run(() =>
+            {
+                var timeSpan = (TimeSpan)methodInfo.Invoke(null, (object[])parameters[closureTemp]);
+                plot.Add(timeSpan.TotalMilliseconds);
+            });
+        }
+    }
+
+    private object[] GenerateParameters(MethodInfo methodInfo, int size)
+    {
+        var parameters = new object[size];
+
+        if (methodInfo.DeclaringType == typeof(TestingMethods))
+        {
+            for (var i = 0; i < size; i++)
+            {
+                parameters[i] = new object[] { GenerateArray(i + 1), i + 1 };
+            }
+        }
+        else
+        {
+            for (var i = 0; i < size; i++)
+            {
+                parameters[i] = new object[] { GenerateMatrix(size), GenerateMatrix(size), i + 1 };
+            }
+        }
+
+        return parameters;
+    }
+
+    private int[] GenerateArray(int size)
+    {
+        var arr = new int[size];
+        var random = new Random();
+
+        for (var i = 0; i < size; i++)
+            arr[i] = random.Next(0, 1000);
+
+        return arr;
+    }
+
+    private int[][] GenerateMatrix(int size)
+    {
+        var matrix = new int[size][];
+
+        for (var i = 0; i < size; i++)
+            matrix[i] = GenerateArray(size);
+
+        return matrix;
+    }
+}
--- AlgsTimeComplexity/ViewModels/ViewModel.cs
+++ AlgsTimeComplexity/ViewModels/ViewModel.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using AlgsTimeComplexity.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 
@@ -13,15 +11,17 @@ public class ViewModel : ObservableObject
 {
     public PlotModel<double> TimePlot { get; set; }
 
-    public RelayCommand CalculateCommand => 
-        new RelayCommand(execute => CalculateParallel(2000));
+    public RelayCommand CalculateCommand { get; set; }
     
     public List<MethodInfo> Methods { get; set; }
     
     public MethodInfo SelectedMethod { get; set; }
 
+    public Calculator Calculator { get; set; }
+
     public ViewModel()
     {
+        Calculator = new Calculator();
         TimePlot = new PlotModel<double>(new ObservableCollection<double>());
         Methods = typeof(TestingMethods).GetMethods(BindingFlags.Public | BindingFlags.Static).ToList();
         Methods.AddRange(
@@ -30,67 +30,9 @@ public class ViewModel : ObservableObject
         
         if (Methods.Count != 0)
             SelectedMethod = Methods[0];
-    }
-
-    private async void CalculateParallel(int maxSize)
-    {
-        if(TimePlot.List.Count != 0)
-            TimePlot.List.Clear();
-
-        for (var size = 1; size < maxSize; size++)
-        {
-            double ms = 0;
-
-            var param = SelectedMethod.DeclaringType == typeof(TestingMethods) 
-                ? new object?[] { GenerateArray(size), size } 
-                : new object?[] { GenerateMatrix(size), GenerateMatrix(size), size };
-            
-            Parallel.Invoke(
-                () =>
-                {
-                    TimeSpan ts = (TimeSpan)SelectedMethod.Invoke(null, param);
-                    ms = ts.TotalMilliseconds;
-                }
+        
+        CalculateCommand = new RelayCommand(
+            execute => Calculator.Calculate(20, SelectedMethod, TimePlot.List)
             );
-
-            await Task.Run(() => { TimePlot.List.Add(ms); });
-        }
-    }
-
-    private List<int> GenerateList(int size)
-    {
-        var list = new List<int>();
-        var random = new Random();
-        
-        if (size is < 0 or > 2000)
-            return list;
-
-        for (var i = 0; i < size; i++)
-        {
-            list.Add(random.Next(0, 1000));
-        }
-
-        return list;
-    }
-
-    private int[] GenerateArray(int size)
-    {
-        var arr = new int[size];
-        var random = new Random();
-
-        for (var i = 0; i < size; i++)
-            arr[i] = random.Next(0, 1000);
-
-        return arr;
-    }
-
-    private int[][] GenerateMatrix(int size)
-    {
-        var matrix = new int[size][];
-        
-        for(var i = 0; i < size; i++)
-            matrix[i] = GenerateArray(size);
-
-        return matrix;
     }
 }
